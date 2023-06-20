0f81785795309d52ef581feea8c23f0c51fd3f47
Brayden Meyer <123148100+brayden-meyer@users.noreply.github.com>
2023-06-20 03:43:08 -0400

    Update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const github = "https://www.github.com/brayden-meyer/";
+const repos = {
+  molview: ["moleculeviewer", "mv"],
+  photos: ["localphoto", "localphotos", "lp"],
+  tc: [],
+  lightweb: ["lw"],
+  snake: ["python", "py"],
+  spaceinvaders: ["si"],
+};
+
+const nextConfig = {
+  async redirects() {
+    const redirects = [];
+
+    for (const [repo, aliases] of Object.entries(repos)) {
+      redirects.push({
+        source: `/${repo}`,
+        destination: `${github}${repo}`,
+        permanent: true,
+      });
+
+      for (const alias of aliases) {
+        redirects.push({
+          source: `/${alias}`,
+          destination: `${github}${repo}`,
+          permanent: true,
+        });
+      }
+    }
+
+    return redirects;
+  },
+};
+
+module.exports = nextConfig;
